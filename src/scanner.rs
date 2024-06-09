4d78e9a416a3a30e35133f0ea3194bbd05f1c2a0
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-06-09 16:49:01 +0100

    refactor: improve readability

--- src/scanner.rs
+++ src/scanner.rs
@@ -74,7 +74,7 @@ impl Scanner {
         #[allow(clippy::enum_glob_use)]
         use super::token::Type::*;
 
-        match self.get_u8() {
+        match self.pop() {
             b'(' => add_token!(self, LeftParen),
             b')' => add_token!(self, RightParen),
             b'{' => add_token!(self, LeftBrace),
@@ -105,27 +105,28 @@ impl Scanner {
     }
 
     fn identifier(&mut self) {
-        while self.peek_u8().is_ascii_alphanumeric() {
-            self.get_u8();
+        while self.peek().is_ascii_alphanumeric() {
+            self.advance();
         }
 
         let bytes = &self.src.get_ref()[self.start..self.current];
         let binding = String::from_utf8(bytes.into()).expect("failed to convert bytes to string");
-        match self.keywords.get(&binding.as_str()) {
-            Some(token_type) => add_token!(self, token_type.to_owned()),
-            None => add_token!(self, token::Type::Identifier),
+        let mut token_type = token::Type::Identifier;
+        if let Some(tt) = self.keywords.get(&binding.as_str()) {
+            tt.clone_into(&mut token_type);
         }
+        add_token!(self, token_type);
     }
 
     fn number(&mut self) {
-        while self.peek_u8().is_ascii_digit() {
-            self.get_u8();
+        while self.peek().is_ascii_digit() {
+            self.advance();
         }
 
-        if self.peek_u8() == b'.' && self.peek_next_u8().is_ascii_digit() {
-            self.get_u8();
-            while self.peek_u8().is_ascii_digit() {
-                self.get_u8();
+        if self.peek() == b'.' && self.peek_next().is_ascii_digit() {
+            self.advance();
+            while self.peek().is_ascii_digit() {
+                self.advance();
             }
         }
 
@@ -135,59 +136,66 @@ impl Scanner {
     }
 
     fn string(&mut self) {
-        while self.peek_u8() != b'"' && self.src.has_remaining() {
-            if self.peek_u8() == b'\n' {
+        while self.peek() != b'"' && self.src.has_remaining() {
+            if self.peek() == b'\n' {
                 self.line += 1;
             }
-            self.get_u8();
+            self.advance();
         }
 
         if !self.src.has_remaining() {
             return Lox::error(self.line, "Unterminated string.");
         }
 
-        self.get_u8();
+        self.advance();
 
         let value = Literal::String(self.string_from_bytes(self.start + 1, self.current - 1));
         add_token!(self, token::Type::String, Some(value));
     }
 
     fn comment(&mut self) {
-        while self.peek_u8() != b'\n' && self.src.has_remaining() {
-            self.get_u8();
+        while self.peek() != b'\n' && self.src.has_remaining() {
+            self.advance();
         }
     }
 
-    fn ignore() {}
+    const fn ignore() {}
 
     fn match_next(&mut self, expected: u8) -> bool {
-        if !self.src.has_remaining() || self.peek_u8() != expected {
-            false
+        if !self.src.has_remaining() || self.peek() != expected {
+            return false;
+        }
+        self.current += 1;
+        self.src.advance(1);
+        true
+    }
+
+    fn peek(&self) -> u8 {
+        if self.src.has_remaining() {
+            self.src.chunk()[0]
         } else {
-            self.current += 1;
-            self.src.advance(1);
-            true
+            b'\0'
         }
     }
 
-    fn get_u8(&mut self) -> u8 {
+    fn peek_next(&self) -> u8 {
+        if self.src.remaining() >= 2 {
+            self.src.chunk()[1]
+        } else {
+            b'\0'
+        }
+    }
+
+    fn advance(&mut self) {
+        // TODO: error if out of bounds
+        self.current += 1;
+        self.src.advance(1);
+    }
+
+    fn pop(&mut self) -> u8 {
         // TODO: error if out of bounds
         self.current += 1;
         self.src.get_u8()
-    }
-
-    fn peek_u8(&self) -> u8 {
-        match self.src.has_remaining() {
-            true => self.src.chunk()[0],
-            false => b'\0',
-        }
-    }
-
-    fn peek_next_u8(&self) -> u8 {
-        match self.src.remaining() {
-            i if i >= 2 => self.src.chunk()[1],
-            _ => b'\0',
-        }
     }
 
     fn string_from_bytes(&self, start: usize, end: usize) -> String {
@@ -223,41 +231,41 @@ mod tests {
     }
 
     #[test]
-    fn test_get_u8() {
+    fn test_pop() {
         let mut scanner = create_scanner();
-        assert_eq!(scanner.get_u8(), b'h');
-        assert_eq!(scanner.get_u8(), b'e');
-        assert_eq!(scanner.get_u8(), b'y');
-        assert_eq!(scanner.get_u8(), b'\n');
+        assert_eq!(scanner.pop(), b'h');
+        assert_eq!(scanner.pop(), b'e');
+        assert_eq!(scanner.pop(), b'y');
+        assert_eq!(scanner.pop(), b'\n');
         // TODO: test that cannot overflow
     }
 
     #[test]
-    fn test_peek_u8() {
+    fn test_peek() {
         let mut scanner = create_scanner();
-        assert_eq!(scanner.peek_u8(), b'h');
-        assert_ne!(scanner.peek_u8(), b'e');
-        scanner.get_u8();
-        assert_eq!(scanner.peek_u8(), b'e');
-        scanner.get_u8();
-        assert_eq!(scanner.peek_u8(), b'y');
-        scanner.get_u8();
-        assert_eq!(scanner.peek_u8(), b'\n');
-        scanner.get_u8();
-        assert_eq!(scanner.peek_u8(), b'\0');
+        assert_eq!(scanner.peek(), b'h');
+        assert_ne!(scanner.peek(), b'e');
+        scanner.pop();
+        assert_eq!(scanner.peek(), b'e');
+        scanner.pop();
+        assert_eq!(scanner.peek(), b'y');
+        scanner.pop();
+        assert_eq!(scanner.peek(), b'\n');
+        scanner.pop();
+        assert_eq!(scanner.peek(), b'\0');
     }
 
     #[test]
-    fn test_peek_next_u8() {
+    fn test_peek_next() {
         let mut scanner = create_scanner();
-        assert_eq!(scanner.peek_next_u8(), b'e');
-        assert_ne!(scanner.peek_u8(), b'l');
-        scanner.get_u8();
-        assert_eq!(scanner.peek_next_u8(), b'y');
-        scanner.get_u8();
-        assert_eq!(scanner.peek_next_u8(), b'\n');
-        scanner.get_u8();
-        assert_eq!(scanner.peek_next_u8(), b'\0');
+        assert_eq!(scanner.peek_next(), b'e');
+        assert_ne!(scanner.peek(), b'l');
+        scanner.pop();
+        assert_eq!(scanner.peek_next(), b'y');
+        scanner.pop();
+        assert_eq!(scanner.peek_next(), b'\n');
+        scanner.pop();
+        assert_eq!(scanner.peek_next(), b'\0');
     }
 
     #[test]
