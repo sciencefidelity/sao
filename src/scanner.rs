60215a6735c75809ba08e338f0cb311b0bb3d8e9
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2024-06-09 12:12:40 +0100

    refactor: test and fix

--- src/scanner.rs
+++ src/scanner.rs
@@ -3,9 +3,9 @@ use bytes::Buf;
 use std::{collections::HashMap, io::Cursor};
 
 macro_rules! add_token {
-    ($self:expr, $typ:expr) => {{
+    ($self:expr, $typ:expr) => {
         add_token!($self, $typ, None)
-    }};
+    };
     ($self:expr, $typ:expr, $lit:expr) => {{
         let text = $self.string_from_bytes($self.start, $self.current);
         $self.tokens.push(Token::new($typ, text, $lit, $self.line));
@@ -93,13 +93,9 @@ impl Scanner {
             b'<' => add_token!(self, Less),
             b'>' if self.match_next(b'=') => add_token!(self, GreaterEqual),
             b'>' => add_token!(self, Greater),
-            b'/' if self.match_next(b'/') => {
-                while self.peek_u8() != b'\n' && self.src.has_remaining() {
-                    self.get_u8();
-                }
-            }
+            b'/' if self.match_next(b'/') => self.comment(),
             b'/' => add_token!(self, Slash),
-            b' ' | b'\r' | b'\t' => {}
+            b' ' | b'\r' | b'\t' => Self::ignore(),
             b'\n' => self.line += 1,
             b'"' => self.string(),
             c if c.is_ascii_digit() => self.number(),
@@ -115,8 +111,7 @@ impl Scanner {
 
         let bytes = &self.src.get_ref()[self.start..self.current];
         let binding = String::from_utf8(bytes.into()).expect("failed to convert bytes to string");
-        let text = binding.as_str();
-        match self.keywords.get(&text) {
+        match self.keywords.get(&binding.as_str()) {
             Some(token_type) => add_token!(self, token_type.to_owned()),
             None => add_token!(self, token::Type::Identifier),
         }
@@ -157,37 +152,120 @@ impl Scanner {
         add_token!(self, token::Type::String, Some(value));
     }
 
+    fn comment(&mut self) {
+        while self.peek_u8() != b'\n' && self.src.has_remaining() {
+            self.get_u8();
+        }
+    }
+
+    fn ignore() {}
+
     fn match_next(&mut self, expected: u8) -> bool {
-        if self.src.has_remaining() || self.peek_u8() == expected {
+        if !self.src.has_remaining() || self.peek_u8() != expected {
+            false
+        } else {
             self.current += 1;
             self.src.advance(1);
             true
-        } else {
-            false
         }
     }
 
     fn get_u8(&mut self) -> u8 {
+        // TODO: error if out of bounds
         self.current += 1;
         self.src.get_u8()
     }
 
     fn peek_u8(&self) -> u8 {
-        if !self.src.has_remaining() {
-            return b'\0';
+        match self.src.has_remaining() {
+            true => self.src.chunk()[0],
+            false => b'\0',
         }
-        self.src.chunk()[0]
     }
 
     fn peek_next_u8(&self) -> u8 {
-        if !self.src.remaining() <= 2 {
-            return b'\0';
+        match self.src.remaining() {
+            i if i >= 2 => self.src.chunk()[1],
+            _ => b'\0',
         }
-        self.src.chunk()[1]
     }
 
     fn string_from_bytes(&self, start: usize, end: usize) -> String {
         let bytes = &self.src.get_ref()[start..end];
         String::from_utf8(bytes.into()).expect("failed to convert bytes to string")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn create_scanner() -> Scanner {
+        let src = Cursor::new(vec![b'h', b'e', b'y', b'\n']);
+        Scanner::new(src)
+    }
+
+    #[test]
+    fn test_match_next() {
+        let mut scanner = create_scanner();
+        let next = scanner.match_next(b'a');
+        assert_eq!(next, false);
+        let next = scanner.match_next(b'h');
+        assert_eq!(next, true);
+        let next = scanner.match_next(b'e');
+        assert_eq!(next, true);
+        let next = scanner.match_next(b'y');
+        assert_eq!(next, true);
+        let next = scanner.match_next(b'\n');
+        assert_eq!(next, true);
+        let next = scanner.match_next(b'\n');
+        assert_eq!(next, false);
+    }
+
+    #[test]
+    fn test_get_u8() {
+        let mut scanner = create_scanner();
+        assert_eq!(scanner.get_u8(), b'h');
+        assert_eq!(scanner.get_u8(), b'e');
+        assert_eq!(scanner.get_u8(), b'y');
+        assert_eq!(scanner.get_u8(), b'\n');
+        // TODO: test that cannot overflow
+    }
+
+    #[test]
+    fn test_peek_u8() {
+        let mut scanner = create_scanner();
+        assert_eq!(scanner.peek_u8(), b'h');
+        assert_ne!(scanner.peek_u8(), b'e');
+        scanner.get_u8();
+        assert_eq!(scanner.peek_u8(), b'e');
+        scanner.get_u8();
+        assert_eq!(scanner.peek_u8(), b'y');
+        scanner.get_u8();
+        assert_eq!(scanner.peek_u8(), b'\n');
+        scanner.get_u8();
+        assert_eq!(scanner.peek_u8(), b'\0');
+    }
+
+    #[test]
+    fn test_peek_next_u8() {
+        let mut scanner = create_scanner();
+        assert_eq!(scanner.peek_next_u8(), b'e');
+        assert_ne!(scanner.peek_u8(), b'l');
+        scanner.get_u8();
+        assert_eq!(scanner.peek_next_u8(), b'y');
+        scanner.get_u8();
+        assert_eq!(scanner.peek_next_u8(), b'\n');
+        scanner.get_u8();
+        assert_eq!(scanner.peek_next_u8(), b'\0');
+    }
+
+    #[test]
+    fn test_string_from_bytes() {
+        let scanner = create_scanner();
+        assert_eq!(
+            scanner.string_from_bytes(scanner.start, scanner.current + 3),
+            "hey"
+        );
     }
 }
